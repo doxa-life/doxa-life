5228c12747223c52e0f1e30b9474892f9f4948a5
corsacca <corsacca@gmail.com>
2026-05-04 13:15:35 +0100

    Change cookie same site method to allow oauth

--- server/api/auth/accept-invite.post.ts
+++ server/api/auth/accept-invite.post.ts
@@ -71,7 +71,7 @@ export default defineEventHandler(async (event) => {
   setCookie(event, 'auth-token', jwtToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: 'lax',
     maxAge: 60 * 60 * 24 * 120
   })
 
--- server/api/auth/register.post.ts
+++ server/api/auth/register.post.ts
@@ -126,7 +126,7 @@ export default defineEventHandler(async (event) => {
     setCookie(event, 'auth-token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'lax',
       maxAge: 60 * 60 * 24 * 120
     })
 
